c6c1823361b22cae3d150c5c945720672b377a56
taewoocode <taewoocode@gmail.com>
2024-12-16 14:40:30 +0900

    first commit

---
+++ src/main/java/com/example/hello_spring_server/HelloSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello_spring_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hello_spring_server/HelloSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_spring_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
